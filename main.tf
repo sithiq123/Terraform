3b8ab9ae14d2f28a6bf8fc425996f13430b4b6e8
abubakkar <abusithiq06@gmail.com>
2023-08-23 20:04:00 +0530

    Second

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "myec2" {
+  ami           = "ami-0d951b011aa0b2c19"
+  instance_type = "t3.micro"
+  count = 3
+  key_name = "abulinux"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
